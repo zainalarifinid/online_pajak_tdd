13ad383247a65a30b04259a83332e55e9cd94f94
Kevin Tanjung <kevintanjung@outlook.com>
2019-02-12 10:45:11 +0700

    feat: give an example of the first Test Case

--- src/StringCalculator.spec.js
+++ src/StringCalculator.spec.js
@@ -2,8 +2,14 @@
 const StringCalculator = require('./StringCalculator');
 
 describe('StringCalculator', () => {
-  it('is a StringCalculator object', () => {
+  it('has an "add" method', () => {
     const stringCalculator = new StringCalculator();
-    expect(stringCalculator).toBeInstanceOf(StringCalculator);
+    expect(stringCalculator.add).toBeDefined();
+    expect(stringCalculator.add).toBeInstanceOf(Function);
+  });
+
+  it('returns zero, given an empty string', () => {
+    const stringCalculator = new StringCalculator();
+    // continue here...
   });
 });
