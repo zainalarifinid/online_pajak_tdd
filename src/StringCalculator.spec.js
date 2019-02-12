d48b4100e85ce2a32ee39db07e7fb5ae03b7027e
Kevin Tanjung <kevintanjung@outlook.com>
2019-02-12 10:14:16 +0700

    Initial commit

---
+++ src/StringCalculator.spec.js
@@ -0,0 +1,9 @@
+
+const StringCalculator = require('./StringCalculator');
+
+describe('StringCalculator', () => {
+  it('is a StringCalculator object', () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator).toBeInstanceOf(StringCalculator);
+  });
+});
