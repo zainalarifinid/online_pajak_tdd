5adb34b0f2f1cdc63eed0bf0baf8f15c239c1963
Zainal Arifin <arifin.1602@gmail.com>
2019-03-13 11:37:33 +0700

    Test TDD Calculator Online Pajak.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,2 +1,65 @@
+module.exports = class StringCalculator {
 
-module.exports = class StringCalculator {}
+
+    isNegative(splitParams){
+        var getNegativeParam =  splitParams.filter(function(data, index){
+            // console.log(data);
+            return data < 0;
+        });
+
+        return getNegativeParam;
+    }
+
+    sum(params, delimiters) {
+        var sum = 0;
+        var splitParams = params.split(delimiters);
+        var dataNegative = this.isNegative(splitParams);
+        if( dataNegative.length == 0 ){
+            for (var i = 0; i < splitParams.length; i++) {
+                if(parseInt(splitParams[i]) < 1000){
+                    sum += parseInt(splitParams[i]);
+                }
+            }
+            return sum;    
+        }else{
+            return `negatives not allowed: ${dataNegative.join()}`
+        }
+
+    }
+
+    add(params) {
+        var process = [',', '\n'];
+        if (params == "") return 0;
+        var splitParams = params.split("");
+        var isComma = (splitParams.indexOf(",") != -1);
+        var isNewLine = (splitParams.indexOf('\n') != -1);
+        var isNewDelimited = (splitParams[0] == "/" && splitParams[1] == "/" );
+        var commaDelimiter = ",";
+        var newLineDelimiter = "\n";
+        // console.log(splitParams);
+        if( isNewDelimited ){
+            // console.log(splitParams);
+            if(isComma) commaDelimiter = splitParams[2];
+            if(isNewLine) newLineDelimiter = splitParams[2];
+        }
+        // console.log(splitParams, process, );
+        
+        if ( !isComma && !isNewLine && isNewDelimited ) return parseInt(params);
+        if ( isComma  && !isNewLine ) {
+            return this.sum(params, commaDelimiter);
+        }
+        // console.log(splitParams);
+        if ( isNewLine  && !isComma ) {
+            return this.sum(params, newLineDelimiter);
+        }
+        if ( isNewLine && isComma ) {
+            var splitComma = params.split(",");
+            var sumAll = 0;
+            for(var i = 0; i< splitComma.length; i++){
+                sumAll += this.add(splitComma[i]);
+            }
+            return sumAll;
+        }        
+    }
+
+}
--- src/StringCalculator.spec.js
+++ src/StringCalculator.spec.js
@@ -11,5 +11,45 @@ describe('StringCalculator', () => {
   it('returns zero, given an empty string', () => {
     const stringCalculator = new StringCalculator();
     // continue here...
+    expect(stringCalculator.add("")).toBe(0);
   });
+
+  it('return the value, given a single number', () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('1')).toBe(1);
+    expect(stringCalculator.add('2')).toBe(2);
+  })
+
+  it('return the sum, give two number with comma delimited', () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('1,2')).toBe(3);
+    expect(stringCalculator.add('10,20')).toBe(30);
+  })
+
+  it('should return the sum, give two number with newline delimited', () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('1\n2')).toBe(3);
+  });
+
+  it("shoudld return sum, given four numbers, with delimited either way", () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('1\n2,3\n4')).toBe(10);
+  })
+
+  it("should return exception with the message, give negative numbers", () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('-5,2')).toBe("negatives not allowed: -5");
+    expect(stringCalculator.add('-1,2,-3')).toBe("negatives not allowed: -1,-3");
+  })
+
+  it("shoukd ignored numbers greater than 1000", () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('1,2,3,1000,1005')).toBe(6);
+  })
+
+  it.only("should change delimited, given a single char on the first line starting", () => {
+    const stringCalculator = new StringCalculator();
+    expect(stringCalculator.add('//#\n1#2')).toBe(3);
+  })
+
 });
